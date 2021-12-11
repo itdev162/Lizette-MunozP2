df10bc91c4952f21a428ce55b9be3e94c478853f
munozl8 <munozl8@gmatc.matc.edu>
2021-12-11 03:26:11 -0600

    Delete function added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,41 +1,79 @@
 import React from 'react';
-import './App.css';
 import axios from 'axios';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Post from './Components/Post/Post';
+import PostList from './Components/PostList/PostList';
+import './App.css';
+
+
+
 
 class App extends React.Component {
+
   state = {
-    posts: []
+    posts: [],
+    post: null
   }
 
   componentDidMount() {
     axios.get('http://localhost:5000/api/posts')
-      .then((response) => {
-        this.setState({
-          posts: response.data
-        })
+    .then((response) => {
+      this.setState({
+        posts: response.data
       })
-      .catch((error) => {
-        console.error(`Error fetching data: ${error}`);
-      })
+    })
+    .catch((error) => {
+      console.error(`Error fetching data: ${error}`);
+    })
   }
 
-  render() {
-    const { posts } = this.state;
+  viewPost = (post) => {
+    console.log(`view ${post.title}`);
+    this.setState({
+      post: post
+    });
+  }
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          Cooking Together
-        </header>
-        <main className="App-content">
-          {posts.map((post: any) =>
-            <div key={post.id}>
-              <h1>{post.title}</h1>
-              <p>{post.body}</p>
-            </div>
-          )}
-        </main>
-      </div>
+  deletePost = post => {
+    axios
+      .delete(`http://localhost:5000/api/posts/${post.id}`)
+      .then(response => {
+        const newPosts = this.state.posts.filter(p => p.id !== post.id);
+        this.setState({
+          posts: [...newPosts]
+        });
+      })
+      .catch(error => {
+        console.error(`Error deleting post: ${error}`);
+      });
+  };
+
+
+  render() {
+    const { posts, post } = this.state;
+
+    return(
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            Cooking Together
+          </header>
+          <main className="App-content">
+            <Switch>
+              <Route exact path="/">
+                <PostList
+                  posts={posts} 
+                  clickPost={this.viewPost}
+                  deletePost={this.deletePost}
+                  />
+              </Route>
+              <Route path="/posts/:postId">
+                <Post post={post} />
+              </Route>
+            </Switch>
+          </main>
+        </div>
+      </Router>
     );
   }
 }
--- client/src/Components/PostList/PostListItem.js
+++ client/src/Components/PostList/PostListItem.js
@@ -1,29 +1,31 @@
 import React from 'react';
-import {useHistory} from 'react-router-dom';
-import slugify from "slugify";
-import './style.css';
+import { useHistory } from 'react-router-dom';
+import slugify from 'slugify';
 
-const PostListItem = props =>{
-    const{post, clickPost} =props;
+
+
+const PostListItem = props => {
+    const { post, clickPost, deletePost } = props;
     const history = useHistory();
 
-    const handleClickPost = post =>{
-        const slug = slugify(post.title,{lower:true});
+    const handleClickPost = post => {
+        const slug = slugify(post.title, { lower: true });
+
         clickPost(post);
         history.push(`/posts/${slug}`);
     };
-return(
-    <div>
-    <div className='postListItem' onClick={()=> handleClickPost(post)}>
-        <h2>{post.title}</h2>
-        <p>{post.body}</p>
-    
-    </div>
-    </div>
-);
 
-
-
-
+    return (
+        <div>
+            <div className="postListItem" onClick={() => handleClickPost(post)}>
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+            </div>
+            <div className="postControls">
+                <button onClick={() => deletePost(post)}>Delete</button>
+            </div>
+        </div>
+    );
 };
+
 export default PostListItem;
